7a845c1a163cad08812e59b06148910b2dff8bb7
Ben (Macbook) <benbromley.104@gmail.com>
2021-01-13 22:37:43 +0000

    added text promts with selection menu, still need to add the selection consequences

--- Test.py
+++ Test.py
@@ -1,9 +1,21 @@
 import Database_Manager
 
-username = input("Username: ")
-password = input("Password: ")
+print("\nWelcome to the sheet music manager \n" 
+      "Please type A, B, or C for what you'd like to do: \n"
+      "[A] Login to Your Account \n"
+      "[B] Delete Your Account \n"
+      "[C] Create a new account \n")
+input("- ")
+auth = "False"
+while auth != "True":
+    username = input("Username: ")
+    password = input("Password: ")
+    test = Database_Manager.UserTable()
+    auth = test.get_user(username, password)
 
-test = Database_Manager.UserTable()
-auth = test.get_user(username, password)
-if auth:
-    print("Access Granted")
+    if auth:
+        break
+    else:
+        print("Access Denied")
+
+print("Access Granted, Welcome", username)
