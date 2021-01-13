9e11a9af78f9eb86a85eff5e6de3992f641f98ba
Ben (Macbook) <benbromley.104@gmail.com>
2021-01-13 17:19:47 +0000

    added comments and create table, create user, and get user is returning true or false based on user details retrieved from the music_database.db

--- Database_Manager.py
+++ Database_Manager.py
@@ -1,38 +1,69 @@
 import sqlite3
 import sql_const
 
-
+# Class for managing the UserTab;e in the database
 class UserTable(object):
     def __init__(self):
+        # Connects to the database file
         self.db_connect = sqlite3.connect('music_database.db')
+        # creates the cursor object
         self.c = self.db_connect.cursor()
-        self.create_database()
+        # see docstring of specified function
+        self.create_table()
 
-    def create_database(self):
+    def create_table(self):
+        '''
+        :return: Creates the user table if it doesn't already exist
+        '''
         with self.db_connect:
             self.c.execute(sql_const.INIT_USER_TABLE)
 
-    # Create user
-    def create_user(self, user_id, username, password):
+    # Creates user
+    def create_user(self, username, password):
+        '''
+        :param username:
+        :param password:
+        :return:
+        '''
         with self.db_connect:
-            self.c.execute(sql_const.CREATE_USER, (user_id, username, password))
+            self.c.execute(sql_const.CREATE_USER, (username, password))
 
-    def delete_user(self, user_id, username, password, library_id):
+    def delete_user(self, username, password):
         with self.db_connect:
-            self.c.execute()
+            # Step 1: Get user ID
+            self.c.execute(sql_const.GET_USER_ID, (username, password))
+            # Step 2: Deleting user
+            self.c.execute(sql_const.DELETE_USER, (username, password))
+            # TODO: Step 3: Remove Users Files
+            # Step 4: Remove Library For User
+            self.c.execute(sql_const.DELETE_USER)
 
-    def auth_user(self, username, password):
+    def get_user(self, username, password):
+        # gets user details from database
         with self.db_connect:
-            self.c.execute()
+            # selects uer record
+            self.c.execute(sql_const.GET_USER, (username, password))
+            # puts result into variable , returns "[]" if details incorrect
+            user_details = self.c.fetchall()
+            # if user_details has data, it prints the details
+            if user_details:
+                return True
+                print(user_details)
+            else:
+                return False
+                print("Login Failed")
 
     def close_database(self):
         # closes database connection
         self.db_connect.close()
 
+class LibraryTable(object):
+    def __init__(self):
+        self.db_connect = sqlite3.connect('music_database.db')
+        self.c = self.db_connect.cursor()
+        self.create_table()
 
-
-'''
-table = UserTable()
-table.create_database()
-'''
+    def create_table(self):
+        with self.db_connect:
+            self.c.execute(sql_const.INIT_LIBRARY_TABLE)
 
